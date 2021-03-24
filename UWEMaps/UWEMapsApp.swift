b869492a32aa2b5e4d25fe05bd3e949b17c138de
= <tomclifford@btinternet.com>
2021-03-24 09:50:32 +0000

    Initial Commit

---
+++ UWEMaps/UWEMapsApp.swift
@@ -0,0 +1,20 @@
+//
+//  UWEMapsApp.swift
+//  UWEMaps
+//
+//  Created by Tom Clifford on 24/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct UWEMapsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
